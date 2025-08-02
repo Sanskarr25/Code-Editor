083ae60feaa6bf44cefafa5aedef606b226a449c
Sanskarr25 <sanskarshete225@gmail.com>
2025-08-02 12:18:15 +0530

    added new main.py file for fastAPI

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+  return "Hello From Server"
